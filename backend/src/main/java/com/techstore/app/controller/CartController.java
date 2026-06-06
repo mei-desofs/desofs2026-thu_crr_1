8533ed9bdccf411253128fa3f25ba527780256da
Chico1201373 <1201373@isep.ipp.pt>
2026-06-06 10:02:19 +0100

    feat: added get cartitem by custumer and implemented frontend for cart page

--- backend/src/main/java/com/techstore/app/controller/CartController.java
+++ backend/src/main/java/com/techstore/app/controller/CartController.java
@@ -2,13 +2,18 @@ package com.techstore.app.controller;
 
 import com.techstore.app.config.ratelimit.annotation.RateLimit;
 import com.techstore.app.dto.cart.CartItemDto;
+import com.techstore.app.dto.cart.CartProductResponseDto;
 import com.techstore.app.dto.cart.UpdateCartItemDto;
 import com.techstore.app.service.interfaces.CartItemService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +27,12 @@ public class CartController {
 
     private final CartItemService cartItemService;
 
+    @GetMapping("/items")
+    public ResponseEntity<List<CartProductResponseDto>> getAllCartItems(HttpServletRequest request) {
+        List<CartProductResponseDto> items = cartItemService.getAllCartItems(request);
+        return ResponseEntity.ok(items);
+    }
+    
     @RateLimit("add-item-to-cart")
     @PostMapping("/items")
     public void addItemToCart(@Valid @RequestBody CartItemDto cartItemDto, HttpServletRequest request) {
@@ -35,6 +46,7 @@ public class CartController {
         cartItemService.updateItemInCart(productId, updateCartItemDto, request);
     }
 
+    @RateLimit("delete-order")
     @DeleteMapping("/items/{productId}")
     public void removeItemFromCart(@PathVariable String productId, HttpServletRequest request) {
         cartItemService.removeItemFromCart(productId, request);
--- backend/src/main/java/com/techstore/app/service/interfaces/CartItemService.java
+++ backend/src/main/java/com/techstore/app/service/interfaces/CartItemService.java
@@ -1,6 +1,9 @@
 package com.techstore.app.service.interfaces;
 
+import java.util.List;
+
 import com.techstore.app.dto.cart.CartItemDto;
+import com.techstore.app.dto.cart.CartProductResponseDto;
 import com.techstore.app.dto.cart.UpdateCartItemDto;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -8,4 +11,6 @@ public interface CartItemService {
     void addItemToCart(CartItemDto cartItemDto, HttpServletRequest request);
     void updateItemInCart(String productId,UpdateCartItemDto cartItemDto, HttpServletRequest request);
     void removeItemFromCart(String productId, HttpServletRequest request);
+    List<CartProductResponseDto> getAllCartItems(HttpServletRequest request);
+
 }
