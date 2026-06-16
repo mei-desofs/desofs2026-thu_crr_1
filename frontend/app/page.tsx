db6883f6c0dfb3e6a36f319feadf7ff78c19bdfe
JoaoPinto1220663 <1220663@isep.ipp.pt>
2026-06-16 12:42:32 +0100

    fix: redirect to home page by role when clicking in techstore logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,13 +2,26 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { apiGet } from "@/lib/api";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
     // Redirect to products page
-    router.push("/products");
+    const redirect = async () => {
+      const me = await apiGet<{ role: string }>("/auth/me");
+      router.refresh();
+      if (me.role === "MANAGER") {
+        router.push("/manager/dashboard");
+      } else if (me.role === "CARRIER") {
+        router.push("/carrier");
+        } else {
+          router.push("/products");
+        }
+        router.refresh();
+    };
+    redirect();
   }, [router]);
 
   return null;
